7616dd29c6e2242562877a3afc7d8b48d3df8872
loudbel <amine.beloud@gmail.com>
2022-10-07 17:09:01 +0100

    last edit task change

--- src/components/Edittask.js
+++ src/components/Edittask.js
@@ -1,6 +1,6 @@
 import {useDispatch} from "react-redux";
 import {editTask} from '../features/todolistSlice'
-import {useEffect,useState} from 'react'
+import {useState} from 'react'
 
 function Edittask(props) {
    const dispatch = useDispatch()
